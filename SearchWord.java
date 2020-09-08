257ab4f42c62fa2d6b9ab0ae08f8cd7dc3ce0eac
Eirik Tobiassen <eiriktob@stud.ntnu.no>
2020-09-08 15:20:25 +0200

    test

--- SearchWord.java
+++ SearchWord.java
@@ -3,36 +3,44 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class SearchWord {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    if (args.length != 2) {
-        System.out.println("Usage: SearchWord <filename> <keyword>");
-        return;
+        if (args.length != 2) {
+            System.out.println("Usage: SearchWord <filename> <keyword>");
+            return;
+        }
+
+        String filename = args[0];
+        String searchw = args[1];
+
+        SearchFileForWords(filename, searchw);
+
     }
 
-    String filename = args[0];
-    String searchw = args[1];
-    int wordCounter = 0;
+    public static void SearchFileForWords(String file, String searchWord) {
 
-    try {
-        FileReader file = new FileReader(filename);
-        Scanner sc = new Scanner(file);
-        while(sc.hasNextLine()) {
-            Scanner check = new Scanner(sc.nextLine());
-            while(check.hasNext()) {
-                if(check.next().toLowerCase().replaceAll("[^a-zA-Z0-9]", "").equals(searchw.toLowerCase())) {
-                    wordCounter++;
+        int wordCounter = 0;
+
+        try {
+            FileReader file = new FileReader(filename);
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                Scanner check = new Scanner(sc.nextLine());
+                while (check.hasNext()) {
+                    if (check.next().toLowerCase().replaceAll("[^a-zA-Z0-9]", "").equals(searchw.toLowerCase())) {
+                        wordCounter++;
+                    }
                 }
             }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            return;
         }
-        sc.close();
-    } 
-    catch (FileNotFoundException e) {
-        System.out.println(e);
-        return;
-    }
-    
-    System.out.println("The keyword '" + searchw + "' appeared " + wordCounter + " times in the " + filename + " file.");
 
-  }
+        System.out.println(
+                "The keyword '" + searchw + "' appeared " + wordCounter + " times in the " + filename + " file.");
+
+    }
+
 }
